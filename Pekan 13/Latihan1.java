8e8042098e5416924fa546142fa81ce683d2e2f8
Riska Khofivah Salsabila <riskakhofivah80@gmail.com>
2026-05-25 21:41:44 +0700

    Create Latihan1.java

---
+++ Pekan 13/Latihan1.java
@@ -0,0 +1,14 @@
+
+public class Latihan1 {
+    
+    int x; 
+
+    public Main() {
+        x = 5;
+    }
+
+    public static void main(String[] args) {
+        Main myObj = new Main(); 
+        System.out.println(myObj.x); 
+    }
+}
