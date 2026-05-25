a732a410d2893bf3118abeb1840618c68c7024b1
Riska Khofivah Salsabila <riskakhofivah80@gmail.com>
2026-05-25 21:42:56 +0700

    Create Latihan2.java

---
+++ Pekan 13/Latihan2.java
@@ -0,0 +1,13 @@
+public class Latihan2 {
+
+    int x;
+  
+    public Main(int nilai) {
+        x = nilai;
+    }
+
+    public static void main(String[] args) {
+        Main obj = new Main(10);
+        System.out.println(obj.x);
+    }
+}
